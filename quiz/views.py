6d38c97dd4e023ea0865e2d662a3be0a7ee4a1f1
AnnaDK <ankrutikova@gmail.com>
2020-08-20 21:39:01 +0000

    pagination created on quiz page. Will display 5 questions per page. Previous Next page function created on Question page. User can swap throug questions

--- quiz/views.py
+++ quiz/views.py
@@ -1,23 +1,42 @@
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404, redirect
+
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, get_object_or_404, render_to_response
 from .models import Quiz
-
 from django.contrib import messages
-
 
 
 def quiz_page(request):
     quizes = Quiz.objects.all()
-   
-    return render(request, "quiz.html", {'quizes': quizes})
+    paginator = Paginator(quizes, 5) # Show 5 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        quizes = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        quizes = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        quizes = paginator.page(paginator.num_pages)
+
+    return render_to_response('quiz.html', {"quizes": quizes})
+
+
 
 
 def quiz_answer(request, pk):
     quiz = get_object_or_404(Quiz, pk=pk)
+    
     if request.method == 'POST':
+        
         messages.success(request, "Answer: ") 
+    
+    
+    nextquestion = Quiz.objects.filter(id__gt=quiz.id).order_by('id').first()
+    previousquestion = Quiz.objects.filter(id__lt=quiz.id).order_by('id').last()
 
-    return render(request, "answer.html", {'quiz': quiz})
+    return render(request, "answer.html", {'quiz': quiz, 'nextquestion': nextquestion, 'previousquestion': previousquestion})
+
 
 
 

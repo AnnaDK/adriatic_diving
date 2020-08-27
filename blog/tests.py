2ae5a0530ccad3fa35a4e3ce98d13865192c7463
AnnaDK <ankrutikova@gmail.com>
2020-08-27 17:16:33 +0000

    check

--- blog/tests.py
+++ blog/tests.py
@@ -1,12 +1,1 @@
 from django.test import TestCase
-from .models import Post
-
-# Create your tests here.
-
-
-class PostTests(TestCase):
-    """tests that we'll run against our activities models """
-
-    def test_str(self):
-        test_name = Post(name='A post')
-        self. assertEqual(str(test_name), 'A post')
